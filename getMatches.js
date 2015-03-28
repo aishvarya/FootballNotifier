f4ed887495687569ae313b19ab34d244a4775c86
Aishvarya Singh <aishvarya@aishvarya-macbookpro.roam.corp.google.com>
2015-03-28 12:31:46 +0100

    Updated files

--- getMatches.js
+++ getMatches.js
@@ -1,37 +1,18 @@
-function getStatusAndTitle(origUrl){
-    var xmlHttp = null;
+function fixurl (origUrl) {
     splitUrl = origUrl.split("/");
-    theUrl = "http://www.espncricinfo.com/netstorage/" + splitUrl[splitUrl.length - 1];
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    txt =  xmlHttp.responseText;
-    var rel = txt.toString();
-    //var re = /<td width="30" align="right"><p class="commsText">[.\n\s\S]*?<tr>/g;
-    re2 = new RegExp(/<p class="statusText">([.\n\s\S]*?)<\/p>/g);
-    ret = re2.exec(rel);
-    cstat = "";
-    if(ret == null)
-        cstat =  "No Status";
-    else if(ret.length == 1)
-        cstat = "No Status";
-    else
-        cstat = ret[1];
-    //console.log(cstat);
-    re2 = new RegExp(/<p class="teamText">([.\n\s\S]*?)<\/p>/g);
-    ret = re2.exec(rel);
-    nmsg = "";
-    if(ret != null)
-        nmsg = '';
-    else
-        nmsg = ret.join(" ");
-    //console.log([cstat, nmsg]);
-    return [cstat, nmsg];
+    theUrl = "http://www.espncricinfo.com/ci/engine/match/" + splitUrl[splitUrl.length - 1].split("?")[0];
+    theUrl = theUrl.replace("html", "json");
+    return theUrl;
+}
+function setStatusAndTitle(origUrl, ele){
+    // console.log("trying to retrieve " + origUrl);
+    $.get(origUrl, function(response) {
+        // console.log("retrieved " + origUrl);
+        ele.innerHTML = response.live.status;
+    });
 };
 function changeDB(cb){
     activeMatches = JSON.parse(localStorage.getItem("activeMatches"));
-    var bkg = chrome.extension.getBackgroundPage();
-    bkg.console.log("activematches");
     if(cb.checked){
         if(!(cb.id in activeMatches)){
             activeMatches.push(cb.id);
@@ -50,71 +31,70 @@ function changeDB(cb){
     }
 }
 function getMatches(){
-	console.log("getmatches");
     if(localStorage.getItem("activeMatches") == null)
         localStorage.setItem("activeMatches", JSON.stringify([]));
-    	console.log("Refreshing");
+    //console.log("Refreshing");
     chrome.tabs.getAllInWindow(
-        function(tab){
-        xmlhttp=new XMLHttpRequest();
-        xmlhttp.open("GET", "http://static.cricinfo.com/rss/livescores.xml", false);
-        xmlhttp.send();
-        matchListXML = xmlhttp.responseXML;
-        matchList = matchListXML.getElementsByTagName("item");
-        concated="";
-        activeMatches = JSON.parse(localStorage.getItem("activeMatches"));
-        if(activeMatches == null)
-            activeMatches = [];
-        newActiveMatches = [];
-        matchURLs = [];
-        for(i=0;i<matchList.length;i++)
-	    matchURLs.push(matchList[i].getElementsByTagName("link")[0]);
-        console.log(matchURLs);
-        //console.log(activeMatches);
-        for(i=0;i<activeMatches.length;i++){
-            if(matchURLs.indexOf(activeMatches[i]) != -1)
-                newActiveMatches.push(activeMatches[i]);                
-        }
-        localStorage.removeItem("activeMatches");
-        localStorage.setItem("activeMatches", JSON.stringify(newActiveMatches));
-        //console.log(newActiveMatches);
-        for(i=0;i<matchList.length;i++){
-            concated += "<h3 value=\"" + i + "\" " + "url=\"" + matchList[i].children[1].textContent + "\"><input class=\"checkbox\" type=\"checkbox\" id=\"" + matchList[i].children[1].textContent + "\"";
-            if(newActiveMatches.indexOf(matchList[i].children[1].textContent) != -1){
-                concated += " checked";
-            }
-            concated += "><label for=\"" + matchList[i].children[1].textContent  + "\">" +  matchList[i].children[0].textContent + "</label></h3><div id=\"status-" + i +  "\" stillLoading=\"1\">Loading...<br/><br/></div>";
-            //concated +="<a href=\"" + matchList[i].children[1].textContent + "\">" + "blastatus"  + "</a>";
-        }
-        document.getElementById('accordion').innerHTML= concated;
-        $('#accordion input[type="checkbox"]').click(function(e) {
-            e.stopPropagation();
-        });
-        $(function() {
-            $( "#accordion" ).accordion({
-                collapsible: true,
-                active : false,
-                heightStyle: "content",
-                activate: function (e, ui) {
-                    val = $(ui.newHeader[0]).attr("value");
-                    url = $(ui.newHeader[0]).attr("url");
-                    currentStatus = document.getElementById("status-" + val);
-                    if(currentStatus != null){
-                        //console.log(currentStatus.innerHTML);
-                        if($(currentStatus).attr("stillLoading") == "1"){
-                            rets = getStatusAndTitle(url);
-                            currentStatus.innerHTML = rets[0];
-                            document.getElementById(url).innerHTML = rets[1];
-                            $(currentStatus).attr("stillLoading",  "0");
-                        }
+            function(tab) {
+                xmlhttp=new XMLHttpRequest();
+                $.get("http://static.cricinfo.com/rss/livescores.xml", false, function(response) {
+                    matchListXML = response;
+                    matchList = matchListXML.getElementsByTagName("item");
+                    concated="";
+                    activeMatches = JSON.parse(localStorage.getItem("activeMatches"));
+                    if(activeMatches == null)
+                        activeMatches = [];
+                    newActiveMatches = [];
+                    matchURLs = [];
+                    for(i=0;i<matchList.length;i++)
+                        matchURLs.push(fixurl(matchList[i].getElementsByTagName("link")[0].textContent));
+                    // console.log(matchURLs);
+                    // console.log(activeMatches);
+                    for(i=0;i<activeMatches.length;i++){
+                        if(matchURLs.indexOf(activeMatches[i]) != -1)
+                            newActiveMatches.push(activeMatches[i]);                
                     }
-                }
-            });
-        });
-        $(".checkbox").change(function() {
-            changeDB(this);
-        })
-    }
+                    localStorage.removeItem("activeMatches");
+                    localStorage.setItem("activeMatches", JSON.stringify(newActiveMatches));
+                    //console.log(newActiveMatches);
+                    for(i=0;i<matchList.length;i++){
+                        link = fixurl(matchList[i].getElementsByTagName("link")[0].textContent);
+                        title = matchList[i].getElementsByTagName("title")[0].textContent;
+                        concated += "<h3 value=\"" + i + "\" " + "url=\"" + link + "\"><input class=\"checkbox\" type=\"checkbox\" id=\"" + link + "\"";
+                        if(newActiveMatches.indexOf(link) != -1){
+                            concated += " checked";
+                        }
+                        concated += "><label for=\"" + link  + "\">" +  title + "</label></h3><div id=\"status-" + i +  "\" stillLoading=\"1\">Loading...<br/><br/></div>";
+                        //concated +="<a href=\"" + matchList[i].children[1].textContent + "\">" + "blastatus"  + "</a>";
+                    }
+                    document.getElementById('accordion').innerHTML= concated;
+                    $('#accordion input[type="checkbox"]').click(function(e) {
+                        e.stopPropagation();
+                    });
+                    $(function() {
+                        $( "#accordion" ).accordion({
+                            collapsible: true,
+                            active : false,
+                            heightStyle: "content",
+                            activate: function (e, ui) {
+                                val = $(ui.newHeader[0]).attr("value");
+                                url = $(ui.newHeader[0]).attr("url");
+                                currentStatus = document.getElementById("status-" + val);
+                                if(currentStatus != null){
+                                    //console.log(currentStatus.innerHTML);
+                                    if($(currentStatus).attr("stillLoading") == "1"){
+                                        setStatusAndTitle(url, currentStatus);
+                                        $(currentStatus).attr("stillLoading",  "0");
+                                    }
+                                }
+                            }
+                        });
+                    });
+                    $(".checkbox").change(function() {
+                        changeDB(this);
+                    })
+                });
+            }
     );
 }
 
